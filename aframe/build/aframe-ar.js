d12667bff45ba100ef30a7d3a8ca82a8efaf0861
Erick Vasilev <rivieravinesa@gmail.com>
2020-03-12 11:54:40 +0800

    Update aframe-ar.js

--- aframe/build/aframe-ar.js
+++ aframe/build/aframe-ar.js
@@ -2530,7 +2530,7 @@ Object.assign(ARjs.Context.prototype, THREE.EventDispatcher.prototype);
 
 // ARjs.Context.baseURL = '../'
 // default to github page
-ARjs.Context.baseURL = 'https://jeromeetienne.github.io/AR.js/three.js/'
+ARjs.Context.baseURL = 'https://github.com/erickvasilev/AR.js/three.js/'
 ARjs.Context.REVISION = '2.2.2';
 
 /**
